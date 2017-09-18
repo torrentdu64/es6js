50733defa82658d98dbef41be4d727fbcbd339c8
torrent benoit <torrentdu64@gmail.com>
2017-09-18 16:38:33 +0200

    destruction

--- syntax.js
+++ syntax.js
@@ -102,6 +102,30 @@ const unshift = (array, ...arg) => [...arg].concat(array);
 //-------------------------------------------------//
 //
 
+const profile = {
+  title: 'Engineer',
+  department: 'Engineering'
+};
+
+const isEngineer = ({title, department}) => title === 'Engineer' && department === 'Engineering';
+
+//-------------------------------------------------//
+//
+
+const classes = [
+  [ 'Chemistry', '9AM', 'Mr. Darnick' ],
+  [ 'Physics', '10:15AM', 'Mrs. Lithun'],
+  [ 'Math', '11:30AM', 'Mrs. Vitalis' ]
+];
+
+const classesAsObject = classes.map( ([subject, time, teacher]) => {
+    return {subject, time, teacher};
+});
+
+//-------------------------------------------------//
+//
+
+
 
 
 
