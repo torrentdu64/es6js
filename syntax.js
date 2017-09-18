b2687924e1a2073459910e9e61e5b8cfb0a19a1d
torrent benoit <torrentdu64@gmail.com>
2017-09-18 17:22:31 +0200

    nested constructor and function

--- syntax.js
+++ syntax.js
@@ -147,7 +147,24 @@ class Monster {
 
 }
 
-
+class Monster {
+  constructor({name}) {
+    this.name = name;
+    this.health = 100;
+  }
+}
+class Snake extends Monster {
+    constructor(options) {
+        super(options);
+    }
+    bite(snake) {
+        return snake.health -= 10;
+    }
+}
+const snake1 = new Snake({name:'rattler'});
+const snake2 = new Snake({name:'copperhead'});
+snake1.bite(snake2);
+console.log(snake2);
 
 
 
